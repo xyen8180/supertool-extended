328b1f34239b301895fe4900423ff987e6150309
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2021-11-14 19:13:09 +0530

    Update Packing-Uploading.sh

--- Packing-Uploading.sh
+++ Packing-Uploading.sh
@@ -2,7 +2,7 @@ PreFix=[BETA-CN]
 ZIPNAME="$(ls ./Input | sed -e 's/\.zip$//')"
 SAMPLE_DEST=./Output/$ZIPNAME
 echo " ===+++ Zipping Files +++==="
-zip -r9 "$ZIPNAME"_by_XD.zip $SAMPLE_DEST
+7z a "$ZIPNAME"_by_XD.zip $SAMPLE_DEST
 echo " ===+++ Uploading Zip +++==="
 rsync -ratlz --rsh="/usr/bin/sshpass -p wLU09QlgSMZhICV ssh -o StrictHostKeyChecking=no -l dargons10" "$ZIPNAME"_by_XD.zip dargons10@frs.sourceforge.net:/home/pfs/project/xdroms/star/$PreFix
 echo " ===+++ Zip Uploaded Sucessfully +++==="
