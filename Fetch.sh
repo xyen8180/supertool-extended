e94b1ac5f99e414216a72499565869d32786fed5
xyen8180 <xyen8180@gmail.com>
2021-11-16 20:13:52 +0530

    B-C 21.11.15

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://hugeota.d.miui.com/V12.5.20.0.RKACNXM/miui_STAR_V12.5.20.0.RKACNXM_942a5712ef_11.0.zip
+ZLINK=https://bigota.d.miui.com/21.11.15/miui_STAR_21.11.15_0c3067c966_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
