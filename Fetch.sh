a5d4e40f614f6d162604840a321d3ec812345239
xyen8180 <xyen8180@gmail.com>
2021-11-25 07:20:20 +0530

    china beta 21-11-24

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://bigota.d.miui.com/21.11.22/miui_STAR_21.11.22_77d5cc6681_12.0.zip
+ZLINK=https://bigota.d.miui.com/21.11.24/miui_STAR_21.11.24_9f82bc2118_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
