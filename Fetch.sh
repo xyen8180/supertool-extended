192e79ca463e82af849f1419ffe14e810ee3a6e9
xyen8180 <xyen8180@gmail.com>
2021-12-19 20:39:04 +0530

    Mi 11 Ultra – EEA Stable V12.5.11.0.RKAEUXM

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://bigota.d.miui.com/V12.5.8.0.RKAMIXM/miui_STARGlobal_V12.5.8.0.RKAMIXM_abce2a9ae6_11.0.zip
+ZLINK=https://bigota.d.miui.com/V12.5.11.0.RKAEUXM/miui_STAREEAGlobal_V12.5.11.0.RKAEUXM_04e2cbde48_11.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
