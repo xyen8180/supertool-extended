59a3996dc6eb848eb947842217d7e63040021591
xyen8180 <xyen8180@gmail.com>
2021-12-28 19:41:58 +0530

    miui 13 21.12.27

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://bigota.d.miui.com/V12.5.11.0.RKAEUXM/miui_STAREEAGlobal_V12.5.11.0.RKAEUXM_04e2cbde48_11.0.zip
+ZLINK=https://bigota.d.miui.com/21.12.27/miui_STARPRE_21.12.27_b9722d12a2_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
