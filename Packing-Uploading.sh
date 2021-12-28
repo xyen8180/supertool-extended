5b97e5d0aa66331b85627467f932be201768ed53
xyen8180 <xyen8180@gmail.com>
2021-12-28 23:17:46 +0530

    spliting zip

--- Packing-Uploading.sh
+++ Packing-Uploading.sh
@@ -4,8 +4,9 @@ SAMPLE_DEST=./Output/$ZIPNAME
 sudo apt-get -y install sshpass
 echo " ===+++ Zipping Files +++==="
 zip -r9 [BOOT]"$ZIPNAME"_by_XD.zip $SAMPLE_DEST/images/boot.img
-zip -r9 [BETA-CN]"$ZIPNAME"_by_XD.zip $SAMPLE_DEST
+zip -r9 -s 3900m [BETA-CN]"$ZIPNAME"_by_XD.zip $SAMPLE_DEST
 echo " ===+++ Uploading Zip +++==="
+rsync -ratlz --rsh="/usr/bin/sshpass -p wLU09QlgSMZhICV ssh -o StrictHostKeyChecking=no -l dargons10" [BETA-CN]"$ZIPNAME"_by_XD.z01 dargons10@frs.sourceforge.net:/home/pfs/project/xdroms/star
 rsync -ratlz --rsh="/usr/bin/sshpass -p wLU09QlgSMZhICV ssh -o StrictHostKeyChecking=no -l dargons10" [BETA-CN]"$ZIPNAME"_by_XD.zip dargons10@frs.sourceforge.net:/home/pfs/project/xdroms/star
 curl -sL [BOOT]"$ZIPNAME"_by_XD.zip https://git.io/file-transfer | sh
 ./transfer wet [BOOT]"$ZIPNAME"_by_XD.zip
