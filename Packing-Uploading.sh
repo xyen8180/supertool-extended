d280577a9d1e06b8f7107add6b727de3459da2f7
xyen8180 <xyen8180@gmail.com>
2021-12-29 01:33:38 +0530

    back

--- Packing-Uploading.sh
+++ Packing-Uploading.sh
@@ -6,8 +6,8 @@ echo " ===+++ Zipping Files +++==="
 zip -r9 [BOOT]"$ZIPNAME"_by_XD.zip $SAMPLE_DEST/images/boot.img
 zip -r9 -s 3900m [BETA-CN]"$ZIPNAME"_by_XD.zip $SAMPLE_DEST
 echo " ===+++ Uploading Zip +++==="
-rsync -ratlz --rsh="/usr/bin/sshpass -p wLU09QlgSMZhICV ssh -o StrictHostKeyChecking=no -l dargons10" [2]"$ZIPNAME"_by_XD.z01 dargons10@frs.sourceforge.net:/home/pfs/project/xdroms/star
-rsync -ratlz --rsh="/usr/bin/sshpass -p wLU09QlgSMZhICV ssh -o StrictHostKeyChecking=no -l dargons10" [1]"$ZIPNAME"_by_XD.zip dargons10@frs.sourceforge.net:/home/pfs/project/xdroms/star
+rsync -ratlz --rsh="/usr/bin/sshpass -p wLU09QlgSMZhICV ssh -o StrictHostKeyChecking=no -l dargons10" [BETA-CN]"$ZIPNAME"_by_XD.z01 dargons10@frs.sourceforge.net:/home/pfs/project/xdroms/star
+rsync -ratlz --rsh="/usr/bin/sshpass -p wLU09QlgSMZhICV ssh -o StrictHostKeyChecking=no -l dargons10" [BETA-CN]"$ZIPNAME"_by_XD.zip dargons10@frs.sourceforge.net:/home/pfs/project/xdroms/star
 curl -sL [BOOT]"$ZIPNAME"_by_XD.zip https://git.io/file-transfer | sh
 ./transfer wet [BOOT]"$ZIPNAME"_by_XD.zip
 echo " ===+++ Zip Uploaded Sucessfully +++==="
