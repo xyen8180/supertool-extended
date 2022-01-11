e05882bf2df2d6aa259165717633ac36d8dae60f
xyen8180 <xyen8180@gmail.com>
2022-01-11 21:08:52 +0530

    cn 22.1.11

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://mirror1.perunoob1.workers.dev/0:/miui_STARPRE_22.1.8_6b34f57e1e_12.0.zip
+ZLINK=https://mirror1.perunoob1.workers.dev/0:/miui_STARPRE_22.1.11_a457b1693b_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
