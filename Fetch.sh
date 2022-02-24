6ffafdc1eb3c9465b498da4925d68d60e958bfbe
xyen8180 <xyen8180@gmail.com>
2022-02-24 19:41:09 +0530

    eea rom

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://mirror1.perunoob1.workers.dev/0:/miui_STAR_V13.0.9.1.9.DEV_47eaadaf47_12.0.zip
+ZLINK=https://mirrorbot.perunoob1.workers.dev/0:/miui_STAREEAGlobal_V13.0.5.0.SKAEUXM_22c3064331_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
