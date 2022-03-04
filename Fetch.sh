9985bdbcb1fcbc1039fc48b6d62d1390f2b20701
xyen8180 <xyen8180@gmail.com>
2022-03-05 00:37:26 +0530

    global new

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://mirror1.perunoob1.workers.dev/0:/miui_STARPRE_22.3.3_70ca7dba79_12.0.zip
+ZLINK=https://mirror1.perunoob1.workers.dev/0:/miui_STARGlobal_V13.0.2.0.SKAMIXM_eecc15ab63_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
