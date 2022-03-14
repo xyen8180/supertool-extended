a23a13dfa0efe153fd0d18b2a6ea785cd1f225fb
xyen8180 <xyen8180@gmail.com>
2022-03-14 19:44:50 +0530

    12 march of china rom

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://mirrorbot.perunoob1.workers.dev/0:/miui_STAR_V13.0.10.1.15.DEV_4e228a14e0_12.0.zip
+ZLINK=https://mirrorbot.perunoob1.workers.dev/0:/miui_STARPRE_22.3.12_442d1df387_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
