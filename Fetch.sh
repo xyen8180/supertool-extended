6e0adee7cdbc751e0168153ef382cbaa8fdbfbcf
xyen8180 <xyen8180@gmail.com>
2022-03-14 20:23:34 +0530

    new group joined 12 march again

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://bigota.d.miui.com/22.3.12/miui_STARPRE_22.3.12_442d1df387_12.0.zip
+ZLINK=https://my.fcgdrive.workers.dev/0:/MirrorBot/miui_STARPRE_22.3.12_442d1df387_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
