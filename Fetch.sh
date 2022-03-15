6260afe1c9c78e36eaa3b3644596b4c3f9ed2dd8
xyen8180 <xyen8180@gmail.com>
2022-03-16 00:26:24 +0530

    a

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=hhttps://drive.google.com/uc?id=1mR_eCeRGp4zdxBbY18oIrEuycyLt9Tiv&export=download
+ZLINK=https://drive.google.com/uc?id=1mR_eCeRGp4zdxBbY18oIrEuycyLt9Tiv&export=download
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
