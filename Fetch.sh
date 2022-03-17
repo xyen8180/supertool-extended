e37f89eb04c466b9d21bf07d8e8ca8b9bf286afb
xyen8180 <xyen8180@gmail.com>
2022-03-17 19:25:56 +0530

    new ghost one

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://www.heliosmirror.live/0:/miui_STARPRE_22.3.16_daec77330c_12.0.zip
+ZLINK=https://ghostmirror.live/0:/miui_STARPRE_22.3.16_daec77330c_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
