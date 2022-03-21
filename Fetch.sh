9756c653f31f1668871173764c3efb142108694f
xyen8180 <xyen8180@gmail.com>
2022-03-21 21:00:40 +0530

    china 18

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://ghostmirror.live/0:/miui_STARPRE_22.3.16_daec77330c_12.0.zip
+ZLINK=https://bigota.d.miui.com/22.3.18/miui_STARPRE_22.3.18_809d9391b2_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
