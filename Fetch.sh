54521fa1ea2ff728a9c6db2a926b1ae7f52d0e51
xyen8180 <xyen8180@gmail.com>
2022-03-30 18:20:58 +0530

    china 29th march china

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://superior-mirror.index-cloud.workers.dev/0:/miui_STAR_V13.0.10.1.18.DEV_a097b660f6_12.0.zip
+ZLINK=https://superior-mirror.index-cloud.workers.dev/0:/miui_STARPRE_22.3.29_cd6f7aec54_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
