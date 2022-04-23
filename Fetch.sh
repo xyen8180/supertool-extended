5b17f5741377db406141c6f0ac2eba958b569f5a
xyen8180 <xyen8180@gmail.com>
2022-04-23 17:39:15 +0530

    dev rom

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://superior-mirror.index-cloud.workers.dev/0:/miui_STARPRE_22.4.21_5c09773c67_12.0.zip
+ZLINK=https://superior-mirror.index-cloud.workers.dev/0:/miui_STAR_V13.0.11.1.27.DEV_25452724a3_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
