80227a15d0a1b65cfc9b9fb4394de9b8a69036e7
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2022-04-29 23:16:14 +0530

    for venus

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://superior.mirror-sandesh.workers.dev/0:/miui_STARPRE_22.4.27_e2ba958e7e_12.0.zip
+ZLINK=https://superior-mirror.index-cloud.workers.dev/0:/miui_VENUS_V13.0.7.1.27.DEV_15155f3827_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
