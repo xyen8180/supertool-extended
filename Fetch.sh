16eb48781be372372321e1ee44c6f808388884ab
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2022-05-25 22:10:08 +0530

    24 may

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://www.mrandom.workers.dev/0:/miui_STARPRE_22.5.23_bd6054ce00_12.0.zip
+ZLINK=https://sakura.rehan23.workers.dev/0:/miui_STARPRE_22.5.24_4b3551dd28_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
