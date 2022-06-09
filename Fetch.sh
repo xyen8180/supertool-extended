54a340053f179f7fc5090add77de00933685665b
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2022-06-09 16:21:41 +0530

    Update Fetch.sh

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://sakura.rehan23.workers.dev/0:/miui_STARPRE_22.6.1_070579638b_12.0.zip
+ZLINK=https://www.savage69.workers.dev/0:/miui_STARINGlobal_V13.0.3.0.SKAINXM_ec136981c6_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
