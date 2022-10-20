57e2442f5b263000486bd58072fb47956acd5fd2
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2022-10-20 10:21:11 +0530

    Update Fetch.sh

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://go.rmzmirror.workers.dev/0:/miui_STARPRE_22.10.8_38ad3cdfa0_12.0.zip
+ZLINK=https://go.shaktirmz.workers.dev/0:/miui_STARPRE_22.10.17_e8736e74fb_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
