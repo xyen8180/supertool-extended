d37e56f336027bccbae44b5e473b00c40b0046ae
xyen8180 <xyen8180@gmail.com>
2022-10-29 13:23:25 +0530

    first rom on my repo

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://go.savagermz.workers.dev/0:/miui_STARPRE_22.10.24_b3b42d0875_12.0.zip
+ZLINK=https://go.savagermz.workers.dev/0:/miui_STARINGlobal_V13.0.5.0.SKAINXM_b4b97ab767_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
