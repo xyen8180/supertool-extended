cdea5a64782358dbdaa26f675ad51be14e53c7e5
xyen8180 <xyen8180@gmail.com>
2022-10-30 19:22:07 +0530

    beta 26.10 cn star

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://go.savagermz.workers.dev/0:/miui_STAR_V13.1.22.9.28.DEV_0889505bbe_12.0.zip
+ZLINK=https://go.savagermz.workers.dev/0:/miui_STARPRE_22.10.26_a0b1860054_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
