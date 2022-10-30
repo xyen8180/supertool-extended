77cb07f37695ae746c9ac45ebb73f7b14ff09ef4
xyen8180 <xyen8180@gmail.com>
2022-10-30 16:18:25 +0530

    prefix fix and xyen change name

--- Packing-Uploading.sh
+++ Packing-Uploading.sh
@@ -4,10 +4,10 @@ SAMPLE_DEST=./Output/$ZIPNAME
 sudo apt-get -y install sshpass
 sudo apt install p7zip
 echo " ===+++ Zipping Files +++==="
-zip -r9 [BOOT]"$ZIPNAME"_by_XD.zip $SAMPLE_DEST/images/boot.img
-7z a -t7z [BETA-CN]"$ZIPNAME"_by_XD.7z $SAMPLE_DEST/*
+zip -r9 [BOOT]"$ZIPNAME"_by_XYEN.zip $SAMPLE_DEST/images/boot.img
+7z a -t7z [ROM]"$ZIPNAME"_by_XYEN.7z $SAMPLE_DEST/*
 echo " ===+++ Uploading Zip +++==="
-curl -sL [BOOT]"$ZIPNAME"_by_XD.zip https://git.io/file-transfer | sh
-./transfer wet [BOOT]"$ZIPNAME"_by_XD.zip
-rsync -ratlz --rsh="/usr/bin/sshpass -p Sourceforge*8180 ssh -o StrictHostKeyChecking=no -l xyen8180" [BETA-CN]"$ZIPNAME"_by_XD.7z xyen8180@frs.sourceforge.net:/home/frs/project/xyen-roms/star
+curl -sL [BOOT]"$ZIPNAME"_by_XYEN.zip https://git.io/file-transfer | sh
+./transfer wet [BOOT]"$ZIPNAME"_by_XYEN.zip
+rsync -ratlz --rsh="/usr/bin/sshpass -p Sourceforge*8180 ssh -o StrictHostKeyChecking=no -l xyen8180" [BETA-CN]"$ZIPNAME"_by_XYEN.7z xyen8180@frs.sourceforge.net:/home/frs/project/xyen-roms/star
 echo " ===+++ Zip Uploaded Sucessfully +++==="
