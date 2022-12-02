5eb142701f042ef10c621e920a96bede5c0a39d8
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2022-12-02 14:47:00 +0530

    Update Fetch.sh

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://go.savagermz.workers.dev/0:/miui_STAREEAVFGlobal_V13.0.11.0.SKAEUVF_dd0d71e096_12.0.zip
+ZLINK=https://go.savagermz.workers.dev/0:/miui_STARIDGlobal_V13.0.5.0.SKAIDXM_9fbd1f6c02_12.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
