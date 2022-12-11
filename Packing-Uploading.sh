14fd88f5ff1cb1914394402a066de1d6116d047b
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2022-12-12 02:24:04 +0530

    trial multisplit

--- Packing-Uploading.sh
+++ Packing-Uploading.sh
@@ -5,9 +5,10 @@ sudo apt-get -y install sshpass
 sudo apt install p7zip
 echo " ===+++ Zipping Files +++==="
 zip -r9 [BOOT]"$ZIPNAME"_by_XYEN.zip $SAMPLE_DEST/images/boot.img
-7z a -t7z [ROM]"$ZIPNAME"_by_XYEN.7z $SAMPLE_DEST/*
+rar a -v4G ./Output/$ZIPNAME_by_XYEN ./Output/$SAMPLE_DEST/*
+rm -rf $SAMPLE_DEST
 echo " ===+++ Uploading Zip +++==="
 curl -sL [BOOT]"$ZIPNAME"_by_XYEN.zip https://git.io/file-transfer | sh
 ./transfer wet [BOOT]"$ZIPNAME"_by_XYEN.zip
-rsync -ratlz --rsh="/usr/bin/sshpass -p Sourceforge*8180 ssh -o StrictHostKeyChecking=no -l xyen8180" [ROM]"$ZIPNAME"_by_XYEN.7z xyen8180@frs.sourceforge.net:/home/frs/project/xyen-roms/star
+rsync -ratlz --rsh="/usr/bin/sshpass -p Sourceforge*8180 ssh -o StrictHostKeyChecking=no -l xyen8180" ./Output/* xyen8180@frs.sourceforge.net:/home/frs/project/xyen-roms/star
 echo " ===+++ Zip Uploaded Sucessfully +++==="
