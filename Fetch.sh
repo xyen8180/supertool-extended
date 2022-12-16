c6a39cef975e7ab665381c0e93c363ad75b7679f
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2022-12-17 01:38:26 +0530

    new update

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://premium.sparrowisland.cf/1:/miui_STAR_V14.0.22.12.5.DEV_7c031b702e_13.0.zip
+ZLINK=https://premium.sparrowisland.cf/1:/miui_STAR_V14.0.22.12.8.DEV_f0435c9e45_13.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
