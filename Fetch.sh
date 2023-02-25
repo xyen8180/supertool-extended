7786fe2c20fc13f504259bc34114d31bcc955805
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2023-02-25 09:31:14 +0530

    Update Fetch.sh

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=http://bigota.d.miui.com/V14.0.23.1.3.DEV/miui_ZEUS_V14.0.23.1.3.DEV_75905dcbaa_13.0.zip
+ZLINK=https://bigota.d.miui.com/V14.0.23.2.6.DEV/miui_ZEUS_V14.0.23.2.6.DEV_694588fdcc_13.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
