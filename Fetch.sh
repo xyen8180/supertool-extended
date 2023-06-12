79f8cf3dead006e2b3651e24205ac8ea5da1e2b7
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2023-06-12 22:40:49 +0530

    Update Fetch.sh

--- Fetch.sh
+++ Fetch.sh
@@ -1,4 +1,4 @@
-ZLINK=https://bigota.d.miui.com/V14.0.23.5.31.DEV/miui_NABU_V14.0.23.5.31.DEV_38094763a5_13.0.zip
+ZLINK=https://bigota.d.miui.com/V14.0.2.0.TKAINXM/miui_STARINGlobal_V14.0.2.0.TKAINXM_260004a39c_13.0.zip
 mkdir Input && mkdir Output
 cd Input
 echo " ===+++ Source Zip Downloading +++==="
